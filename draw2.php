41099a3ed7b26842c179a233c1865f5f6901d16d
ChristineScoot <skotnicznyk@gmail.com>
2018-03-09 12:13:32 +0100

    Update draw2.php

--- draw2.php
+++ draw2.php
@@ -2,7 +2,7 @@
 <?php
     $name=$_POST['name'];
     $number=rand(1,1000);
-    $id_draw=rand(6,13);
+    $id_draw=rand(1,9);
     $conn=new mysqli('localhost', 'root', '', 'fant');
     if($name AND $number AND $id_draw){
         $result=$conn->query("INSERT INTO user values ('', '$name', '$number', '$id_draw');");
